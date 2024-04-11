5dba36b1d57bc8114fa730f046d521d7e34330d1
imran <isoteriksoftware@gmail.com>
2024-04-11 10:40:25 +0100

    updated generators

--- src/core/utils/index.ts
+++ src/core/utils/index.ts
@@ -1,6 +1,11 @@
 import { ParticleGeometryGenerator, ParticleMaterialGenerator } from "../../components";
 import * as THREE from "three";
 
+/**
+ * Returns a default particle geometry generator function.
+ * This generator preserves and reuses a single geometry for all particles.
+ * @returns A particle geometry generator function.
+ */
 export const getDefaultParticleGeometryGenerator = (): ParticleGeometryGenerator => {
   let geometry: THREE.PlaneGeometry;
 
@@ -15,24 +20,100 @@ export const getDefaultParticleGeometryGenerator = (): ParticleGeometryGenerator
   return generator;
 };
 
+/**
+ * Returns a default particle material generator function.
+ * This generator creates and reuses materials based on the provided textures and color.
+ * @returns A particle material generator function.
+ */
 export const getDefaultParticleMaterialGenerator = (): ParticleMaterialGenerator => {
-  let material: THREE.MeshLambertMaterial;
+  let materials: THREE.MeshLambertMaterial[];
 
-  const generator: ParticleMaterialGenerator = (_, textures, { opacity, color }) => {
-    if (!material) {
-      material = new THREE.MeshLambertMaterial({
-        map: textures[Math.floor(Math.random() * textures.length)],
+  const generator: ParticleMaterialGenerator = (index, textures, { opacity, color }) => {
+    if (!materials) {
+      materials = [];
+
+      for (let i = 0; i < textures.length; i++) {
+        materials.push(
+          new THREE.MeshLambertMaterial({
+            map: textures[i],
+            transparent: true,
+            opacity: opacity,
+            depthWrite: false,
+            color: color,
+            polygonOffset: true,
+            polygonOffsetFactor: 1,
+            polygonOffsetUnits: 1,
+          }),
+        );
+      }
+    }
+
+    return materials[index % materials.length];
+  };
+
+  return generator;
+};
+
+/**
+ * Returns a particle material generator function that generates materials with multiple colors.
+ * The total number of materials generated can be based on either the number of colors, textures, or a specified density.
+ * @param colors An array of colors to be used for the materials.
+ * @param sizeDeterminant Determines how many materials to generate based on the colors, textures, or a specified density.
+ * @returns A particle material generator function.
+ */
+export const getMultiColorParticleMaterialGenerator = (
+  colors: [THREE.Color, THREE.Color, ...rest: THREE.Color[]],
+  sizeDeterminant: "colors" | "textures" | "density" = "textures",
+): ParticleMaterialGenerator => {
+  let materials: THREE.MeshLambertMaterial[];
+
+  const generator: ParticleMaterialGenerator = (index, textures, { opacity, density }) => {
+    if (!materials) {
+      materials = [];
+
+      const commonProps = {
         transparent: true,
         opacity: opacity,
         depthWrite: false,
-        color: color,
         polygonOffset: true,
         polygonOffsetFactor: 1,
         polygonOffsetUnits: 1,
-      });
+      };
+
+      if (sizeDeterminant === "textures") {
+        for (let i = 0; i < textures.length; i++) {
+          materials.push(
+            new THREE.MeshLambertMaterial({
+              map: textures[i],
+              color: colors[i % colors.length],
+              ...commonProps,
+            }),
+          );
+        }
+      } else if (sizeDeterminant === "colors") {
+        for (let i = 0; i < colors.length; i++) {
+          materials.push(
+            new THREE.MeshLambertMaterial({
+              map: textures[i % textures.length],
+              color: colors[i],
+              ...commonProps,
+            }),
+          );
+        }
+      } else {
+        for (let i = 0; i < density; i++) {
+          materials.push(
+            new THREE.MeshLambertMaterial({
+              map: textures[i % textures.length],
+              color: colors[i % colors.length],
+              ...commonProps,
+            }),
+          );
+        }
+      }
     }
 
-    return material;
+    return materials[index % materials.length];
   };
 
   return generator;
